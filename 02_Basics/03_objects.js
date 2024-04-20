c605f7a678f7c0b5b22035dfe2daf79cd101a6b3
Shahria Kabir <shahriakabir@Kabirs-Mac.local>
2024-04-20 08:31:43 +0600

    ObjectUp

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,12 @@
+// object literals
+const JsUser = {
+  name: "Shahria",
+  fullName: "Shahria Kabir",
+  age: 23,
+  location: "BD",
+  email: "99skabir@gmail.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Saturday", "Monday"],
+};
+console.log(JsUser.email);
+console.log(JsUser["fullName"]);
