90c67142581726109c83a69ae694daeffe721d50
Shahria Kabir <shahriakabir@Kabirs-Mac.local>
2024-04-25 22:21:47 +0600

    Learining

---
+++ script.js
@@ -0,0 +1,106 @@
+// Unary Operators
+/*
+let a = 5;
+let b = 2;
+
+//a++; //increment
+//b--; //decrement
+console.log("a++ =", a++);
+console.log(a);
+*/
+
+//Assignment Operators
+/*
+let a = 5;
+let b = 2;
+
+a += 4; // a = a + 4;
+console.log("a =", a); //9
+*/
+
+// Comparison Operators
+/*
+let a = 5;
+let b = "5"; //string -> number
+
+console.log("a == b", a == b);
+console.log("a > b", a > b);
+*/
+
+//logical Operators
+//( && , ||, ! ) || -> pipe symble
+/*
+let a = 5;
+let b = 2;
+
+let cond1 = a > b; //true
+let cond2 = a === 5; //true
+
+console.log("cond1 && cond2 =", cond1 && cond2);
+*/
+
+// Conditional statements
+/*
+let age = 25;
+
+if (age > 18) {
+  console.log("You can Vote");
+}
+*/
+
+// odd or even ?
+/*
+let num = 13;
+if (num % 2 === 0) {
+  console.log(num, "is even");
+} else {
+  console.log(num, "is odd");
+}
+*/
+
+// else if statements
+/*
+let mode = "dark";
+let color;
+
+if (mode === "dark") {
+  color = "black";
+} else if (mode === blue) {
+  color = "blue";
+} else if (mode === pink) {
+  color = "pink";
+} else {
+  color = "white";
+}
+console.log(color);
+*/
+
+// Ternary operator
+//-> a ? b : c;
+//alternative of if else operator
+/*
+let age = 25;
+let result = age >= 18 ? "Adult" : "not adult";
+console.log(result);
+*/
+
+/*
+let num = prompt("enter a number");
+if (num % 5 === 0) {
+  console.log(num, "is multiple of 5");
+} else {
+  console.log(num, "is not a multiple of 5");
+}
+*/
+
+/*
+let score = prompt("enter a number");
+let grade;
+if (score >= 80 && score <= 100) {
+  grade = "A+";
+} else {
+  grade = "F";
+}
+
+console.log(grade);
+*/
